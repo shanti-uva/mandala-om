23e42ba8a5b359ce863bbdb314df50fed9bbda95
ys2n <ys2n@virginia.edu>
2020-06-08 10:55:37 -0400

    Push gh-pages changes

---
+++ kmaps-app/build/precache-manifest.9f36b8e69eda7fadb677957f1b5f0bd2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a999d1f7c6dfa3ae3f9112e28e7a879",
+    "url": "/mandala-om/index.html"
+  },
+  {
+    "revision": "316b786bf9e960c3f8e0",
+    "url": "/mandala-om/static/css/2.0f258c89.chunk.css"
+  },
+  {
+    "revision": "c54d7487eac537badd48",
+    "url": "/mandala-om/static/css/main.1f861812.chunk.css"
+  },
+  {
+    "revision": "316b786bf9e960c3f8e0",
+    "url": "/mandala-om/static/js/2.cbd0f88c.chunk.js"
+  },
+  {
+    "revision": "95d1ce41cc82be7ee7d9538dd6e56067",
+    "url": "/mandala-om/static/js/2.cbd0f88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54d7487eac537badd48",
+    "url": "/mandala-om/static/js/main.638dbc98.chunk.js"
+  },
+  {
+    "revision": "0566f1f65a8b9eb55866",
+    "url": "/mandala-om/static/js/runtime-main.5255fc5b.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/mandala-om/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "9d6d13f8c9b86e3c48f65a569fc1b114",
+    "url": "/mandala-om/static/media/shanticon.9d6d13f8.ttf"
+  },
+  {
+    "revision": "bcb9b6f157a6bdcbcefc6888ab285d6d",
+    "url": "/mandala-om/static/media/treebuts.bcb9b6f1.png"
+  }
+]);
