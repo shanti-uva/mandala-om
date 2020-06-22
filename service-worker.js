72d5802a617db2f53d8014cb3e38f2014b018e1c
ys2n <ys2n@virginia.edu>
2020-06-22 12:27:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mandala-om/precache-manifest.5f65897a7c9de2bbba349479164ec959.js"
+  "/mandala-om/precache-manifest.f2da77c7318ebb8eb420e16befcc58a7.js"
 );
 
 self.addEventListener('message', (event) => {
