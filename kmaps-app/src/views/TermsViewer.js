a76056cb97d77e6d6b63520ca996560546bb25e1
Gerard Ketuma <gketuma@gmail.com>
2020-06-25 15:29:22 -0500

    Update dependencies by adding prettier for consistent file formatting on pre-commit hooks.

--- kmaps-app/src/views/TermsViewer.js
+++ kmaps-app/src/views/TermsViewer.js
@@ -1,48 +1,51 @@
 import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Definitions from './TermsViewer_Definitions';
-import TermNames from "./TermsViewer_Names";
-import TermAudioPlayer from "./TermsViewer_AudioPlayer";
-import RelatedTerms from "./TermsViewer_RelatedTerms";
+import TermNames from './TermsViewer_Names';
+import TermAudioPlayer from './TermsViewer_AudioPlayer';
+import RelatedTerms from './TermsViewer_RelatedTerms';
 // import CardGroup from "react-bootstrap/CardGroup";
 import 'rc-input-number/assets/index.css';
-import NodeHeader from "./common/NodeHeader";
-import {FeatureGallery} from "./common/FeatureGallery";
-import {RelatedsGallery} from "./common/RelatedsGallery";
+import NodeHeader from './common/NodeHeader';
+import { FeatureGallery } from './common/FeatureGallery';
+import { RelatedsGallery } from './common/RelatedsGallery';
 
 // import KmapContext from "../context/KmapContext";
 
 // Bootstrap
 
 export default function TermsViewer(props) {
-
-    let output = <div className={'termsviewer'}>
-        Loading...
-    </div>;
+    let output = <div className={'termsviewer'}>Loading Test...</div>;
     if (props.kmasset && props.kmasset.asset_type) {
-        output =
+        output = (
             <div className={'termsviewer'}>
-                <div className={"sui-terms"}>
-                    <NodeHeader kmasset={props.kmasset}/>
+                <div className={'sui-terms'}>
+                    <NodeHeader kmasset={props.kmasset} />
                     <Switch>
-                        <Route path={'/view/:viewerType/:id/related/:relatedType/:viewMode'}>
-                            <RelatedsGallery {...props}/>
+                        <Route
+                            path={
+                                '/view/:viewerType/:id/related/:relatedType/:viewMode'
+                            }
+                        >
+                            <RelatedsGallery {...props} />
                         </Route>
-                        <Route path={'/view/:viewerType/:id/related/:relatedType'}>
-                            <Redirect to={"./all"}/>
+                        <Route
+                            path={'/view/:viewerType/:id/related/:relatedType'}
+                        >
+                            <Redirect to={'./all'} />
                         </Route>
                         <Route>
-                            <TermNames kmap={props.kmap}/>
-                            <TermAudioPlayer kmap={props.kmap}/>
-                            <Definitions kmap={props.kmap}/>
-                            <RelatedTerms kmap={props.kmap}/>
+                            <TermNames kmap={props.kmap} />
+                            <TermAudioPlayer kmap={props.kmap} />
+                            <Definitions kmap={props.kmap} />
+                            <RelatedTerms kmap={props.kmap} />
                         </Route>
                     </Switch>
                 </div>
                 {/*<pre>{JSON.stringify(props.kmap, undefined, 2)}</pre>*/}
             </div>
+        );
     }
 
     return output;
 }
-
