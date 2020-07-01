4b1bdf5ff05802959e7e6c7f9f7b3a39ab5e0398
ys2n <ys2n@virginia.edu>
2020-07-01 14:11:48 -0400

    start using PUBLIC_URL environment variable instead of hardcoded path

--- kmaps-app/src/main/TopBar.js
+++ kmaps-app/src/main/TopBar.js
@@ -1,11 +1,17 @@
-import {Link} from "react-router-dom";
-import React from "react";
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 export function TopBar() {
-    const topBar = <div className={'sui-topBar'}>
-        <Link to={'/home'}>
-            <img src={'/mandala-om/img/bhutanleft.gif'} style={{cursor: 'pointer'}} alt={'Home Page'} />
-        </Link>
-    </div>
+    const topBar = (
+        <div className={'sui-topBar'}>
+            <Link to={'/home'}>
+                <img
+                    src={process.env.PUBLIC_URL + '/img/bhutanleft.gif'}
+                    style={{ cursor: 'pointer' }}
+                    alt={'Home Page'}
+                />
+            </Link>
+        </div>
+    );
     return topBar;
 }
