b41bcc517fb0ec49363bebc3b75f6b9ae5e97308
ys2n <ys2n@virginia.edu>
2020-07-14 21:19:34 -0400

    Updates

---
+++ precache-manifest.4b1cc8073ec875cc9a8ddd28d381b756.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db844c12b9d1cfb978c723ccdf75be0",
+    "url": "/mandala-om/index.html"
+  },
+  {
+    "revision": "9aeb485212924f94919f",
+    "url": "/mandala-om/static/css/2.f83d61e1.chunk.css"
+  },
+  {
+    "revision": "890ba8480bef73a6f060",
+    "url": "/mandala-om/static/css/main.7b82b49d.chunk.css"
+  },
+  {
+    "revision": "9aeb485212924f94919f",
+    "url": "/mandala-om/static/js/2.f88f8b3c.chunk.js"
+  },
+  {
+    "revision": "1ab7cf4620599533a111f6e6b073b6d5",
+    "url": "/mandala-om/static/js/2.f88f8b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890ba8480bef73a6f060",
+    "url": "/mandala-om/static/js/main.994bf61b.chunk.js"
+  },
+  {
+    "revision": "0566f1f65a8b9eb55866",
+    "url": "/mandala-om/static/js/runtime-main.5255fc5b.js"
+  },
+  {
+    "revision": "1b36d9cefdb158d339a2242450fe3a30",
+    "url": "/mandala-om/static/media/museosans-300-webfont.1b36d9ce.svg"
+  },
+  {
+    "revision": "2b6eff479f7af3b47ea6f22eea51194b",
+    "url": "/mandala-om/static/media/museosans-300-webfont.2b6eff47.eot"
+  },
+  {
+    "revision": "4370e8d764a527255fc5f922fabb28d2",
+    "url": "/mandala-om/static/media/museosans-300-webfont.4370e8d7.ttf"
+  },
+  {
+    "revision": "80e98e28fc0d4da6661f907ebe0a7963",
+    "url": "/mandala-om/static/media/museosans-300-webfont.80e98e28.woff"
+  },
+  {
+    "revision": "b08f74892d94e027ca58c30ef41d7d75",
+    "url": "/mandala-om/static/media/museosans-300-webfont.b08f7489.woff2"
+  },
+  {
+    "revision": "9d6d13f8c9b86e3c48f65a569fc1b114",
+    "url": "/mandala-om/static/media/shanticon.9d6d13f8.ttf"
+  },
+  {
+    "revision": "bcb9b6f157a6bdcbcefc6888ab285d6d",
+    "url": "/mandala-om/static/media/treebuts.bcb9b6f1.png"
+  }
+]);
