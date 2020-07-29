2344e637d95c4e05fff206b7b551a32bc306f7d8
ys2n <ys2n@virginia.edu>
2020-07-29 17:37:09 -0400

    A better Dummy Component for ImagesViewer.js

--- kmaps-app/src/views/ImagesViewer.js
+++ kmaps-app/src/views/ImagesViewer.js
@@ -1,44 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useStatus from '../hooks/useStatus';
 
-export class ImagesViewer extends React.Component {
-    constructor(props) {
-        super(props);
+export function ImagesViewer(props) {
+    const status = useStatus();
 
-        console.error(typeof props.sui);
-        if (typeof props.sui !== 'object') {
-            throw new Error(
-                'sui must be passed as a property to the component!'
-            );
-        }
+    useEffect(() => {
+        console.log(props);
+        status.clear();
+        status.setHeaderTitle('Image Viewer For A Better Tomorrow');
+        status.setType('images');
+    });
 
-        if (typeof props.sui.pages !== 'object') {
-            throw new Error(
-                'sui.pages must be passed as part of the sui passed to the constructor!'
-            );
-        }
-
-        this.sui = props.sui;
-        this.props = props;
-
-        console.log('ImagesViewer props = ', props);
-    }
-
-    componentDidMount() {}
-
-    componentDidCatch(error, errorInfo) {}
-
-    componentWillUnmount() {}
-
-    componentDidUpdate(prevProps, prevState, snapshot) {}
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {}
-
-    render() {
-        return (
-            <div class={'images legacy'}>
-                NOT YET IMPLEMENTED IMAGES LEGACY{' '}
-                <pre>{JSON.stringify(this.props.kmasset)}</pre>
-            </div>
-        );
-    }
+    return (
+        <div className={'images legacy'}>
+            NOT YET IMPLEMENTED IMAGES LEGACY{' '}
+            <pre>{JSON.stringify(props.kmasset)}</pre>
+        </div>
+    );
 }
