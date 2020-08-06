6859972defd19a1643fcbf8961c235a5c0325c42
ys2n <ys2n@virginia.edu>
2020-08-06 17:03:31 -0400

    Cleaned up demo links on the Home page

--- kmaps-app/src/main/Home.js
+++ kmaps-app/src/main/Home.js
@@ -7,38 +7,87 @@ export function Home(props) {
             <h2>Home</h2>
 
             <h3>Work-in-Progress</h3>
+
+            <h4>Terms</h4>
             <ul>
                 <li>
                     <Link to={'/terms/terms-12434'}>/terms/terms-12434</Link>
                 </li>
                 <li>
-                    <Link to={'/terms/terms-45057'}>/terms/terms-45057</Link>
+                    <Link to={'/terms/terms-45057'}>
+                        'gro ba/: /terms/terms-45057
+                    </Link>
                 </li>
                 <li>
-                    <Link to={'/terms/terms-85193'}>/terms/terms-85193</Link>
+                    <Link to={'/terms/terms-85193'}>
+                        thod rgal/: /terms/terms-85193
+                    </Link>
+                </li>
+            </ul>
+            <h4>Places</h4>
+            <ul>
+                <li>
+                    <Link to={'/places/places-16408'}>
+                        {' '}
+                        Potala Palace: /places/places-16408
+                    </Link>
                 </li>
                 <li>
-                    <Link to={'/terms/places-16408'}>/terms/places-16408</Link>
+                    <Link to={'/places/places-637'}>
+                        {' '}
+                        Lhasa: /places/places-637
+                    </Link>
                 </li>
+                Inline Viewers:
+                <ul>
+                    <li>
+                        <Link to={'/places/places-637/related-images/deck'}>
+                            {' '}
+                            Related Images
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to={'/places/places-637/related-audio-video/deck'}
+                        >
+                            {' '}
+                            Related Audio-Video
+                        </Link>
+                    </li>
+                </ul>
+            </ul>
+            <h4>Search</h4>
+            <ul>
                 <li>
                     <Link to={'/search'}>/search</Link>
                 </li>
+            </ul>
+
+            <h4>Texts</h4>
+            <ul>
                 <li>
                     <Link to={'/texts/16230'}>
                         Text: Introduction to Drepung Colleges
                     </Link>
                 </li>
                 <li>
-                    <Link to={'/texts/63401'}>Text: Bogus Multi Page Doc</Link>
+                    <Link to={'/texts/63401'}>Text: Bogus Multi Page Doc</Link>{' '}
+                    has popover examples
                 </li>
                 <li>
-                    <Link to={'/texts/46641'}>Text: Veronoica’s Test</Link>
+                    <Link to={'/texts/46641'}>Text: Veronoica’s Test</Link> has
+                    embedded video etc.
                 </li>
                 <li>
                     <Link to={'/texts/50751'}>
                         Text: Hagar of The Pawn-Shop
-                    </Link>
+                    </Link>{' '}
+                    has Hagar... and a Pawn Shop.
                 </li>
+            </ul>
+
+            <h4>Audio-Video</h4>
+            <ul>
                 <li>
                     <Link to={'/audio-video/256'}>
                         Video: Women Discuss Their Dreams
@@ -48,26 +97,6 @@ export function Home(props) {
                     <Link to={'/audio-video/306'}>
                         Video: Dawa and Pudrön Flirt
                     </Link>
-                </li>
-            </ul>
-
-            <h3>Semi-functional Asset Viewers</h3>
-            <ul>
-                <li>
-                    <Link to={'/assets/terms-85193'}>/assets/terms-85193</Link>{' '}
-                    (Legacy)
-                </li>
-            </ul>
-
-            <h3>Dys-functional legacy viewer</h3>
-            <ul>
-                <li>
-                    <Link to={'/assets/subjects-6752'}>
-                        /assets/subjects-6752
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/assets/places-637'}>/assets/places-637</Link>
                 </li>
             </ul>
 
