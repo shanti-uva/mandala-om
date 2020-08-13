fe45d0342471b5b43f8886b9418a504827542010
ys2n <ys2n@virginia.edu>
2020-08-13 16:23:44 -0400

    HistoryViewer:  Related Assets in history

--- kmaps-app/src/views/History/HistoryViewer.js
+++ kmaps-app/src/views/History/HistoryViewer.js
@@ -13,21 +13,27 @@ import './HistoryViewer.css';
 function HistoryLocation(props) {
     const { removeLocation } = useStoreActions((actions) => actions.history);
     const history = useHistory();
+    // console.log("HistoryLocation location: ", props.location);
+
+    const loc = props.location?.relTitle ? (
+        props.location.relTitle
+    ) : (
+        <span className={'c-HistoryViewer__title'}>
+            <span
+                className={
+                    'icon shanticon-' + props.location?.asset_type || 'shanti'
+                }
+            ></span>{' '}
+            {props.location?.name}
+        </span>
+    );
 
     return (
         <div
             className="c-HistoryViewer__relatedRecentItem"
             onClick={(event) => history.push(props.location.pathname)}
         >
-            <span to={props.location.pathname}>
-                <span
-                    className={
-                        'icon shanticon-' + props.location.asset_type ||
-                        'shanti'
-                    }
-                ></span>{' '}
-                {props.location.name}
-            </span>
+            {loc}
             <span
                 className="c-HistoryViewer__removeItem shanticon-cancel-circle icon"
                 data-key={props.location.key}
@@ -54,48 +60,95 @@ export function HistoryViewer(props) {
         (actions) => actions.history
     );
     const HISTORY_LENGTH = 25;
-    // const histRef = useRef(new Map());
-    // const [historyStack] = useState(histRef);
     const location = useLocation();
-    const match = useRouteMatch('/:type/:id');
+    const match = useRouteMatch([
+        '/:type/:id/related-:relType/view/:relId',
+        '/:type/:id',
+    ]);
     // console.log('HISTORY VIEWER current = ', location);
     // console.log('HISTORY VIEWER match = ', match);
 
-    const [type, fixedId] = match.params.id.split('-');
+    const relatedType = match.params.relType;
+    const relatedId = match.params.relId;
+
+    function fixId(theId) {
+        if (!theId) {
+            return null;
+        }
+        const [fixedId] = theId.match(/(\d+)$/);
+        return fixedId;
+    }
+
+    const fixedId = fixId(match.params.id);
     const kmasset = useAsset(match.params.type, fixedId);
 
+    const fixedRelId = fixId(relatedId);
+    const relAsset = useAsset(relatedType, fixedRelId);
+
     // console.log(' HISTORY kmasset from kmapid = (', fixedId, ') is ', kmasset);
+    // console.log(' HISTORY related relatedType: ', relatedType, " related id: ", relatedId);
+    // console.log(' HISTORY related asset: ', relAsset);
+
+    function fixTitle(docs) {
+        let title = null;
+        if (docs?.length) {
+            title = docs[0].title;
+            if (!title) {
+                title = docs[0].caption;
+            }
+        }
+        if (!title) {
+            title = null;
+        }
+
+        // console.log("grokked title = ", title, " from ", { title: docs[0].title, caption: docs[0].caption });
+        return title;
+    }
 
     useEffect(() => {
         if (kmasset) {
             if (kmasset.numFound > 0) {
-                console.log('HISTORY VIEWER: kmasset = ', kmasset);
-                const title = kmasset.docs[0]?.title?.length
-                    ? kmasset.docs[0]?.title
-                    : 'Unknown';
+                // console.log('HISTORY VIEWER: kmasset = ', kmasset);
+
+                // Clean up title and related
+                const title = fixTitle(kmasset.docs);
+                const relLabel = fixTitle(relAsset.docs);
+
+                const relTitle = relatedType ? (
+                    <span
+                        className={
+                            'c-HistoryViewer__title c-HistoryViewer__relatedTitle'
+                        }
+                    >
+                        <span
+                            className={`icon shanticon-${relatedType}`}
+                        ></span>{' '}
+                        {relLabel}
+                    </span>
+                ) : null;
+
+                // Create base location
                 const baseLocation = {
                     ...location,
                     name: title,
+                    relTitle: relTitle,
                     pathname: match.url,
                     asset_type: kmasset?.docs[0].asset_type,
                     kmasset: kmasset?.docs[0],
                 };
-                console.log(
-                    'Making history: location = ',
-                    location,
-                    ' kmasset = ',
-                    kmasset,
-                    ' match.url = ',
-                    match.url
-                );
+
+                // console.log(
+                //     'Making history: location = ',
+                //     location,
+                //     ' kmasset = ',
+                //     kmasset,
+                //     ' match.url = ',
+                //     match.url
+                // );
                 addLocation(baseLocation);
             }
         }
-    }, [kmasset]);
-
-    // useEffect( () => {
-    //     clear();
-    // },[]);
+    }, [kmasset, relAsset]);
 
     let historyList = [];
     historyStack.forEach((location, key) => {
--- kmaps-app/src/views/SearchViewer.js
+++ kmaps-app/src/views/SearchViewer.js
@@ -5,13 +5,23 @@ import { useStoreState } from 'easy-peasy';
 
 export function SearchViewer(props) {
     const status = useStatus();
+
     const loadingState = useStoreState((state) => state.search.loadingState);
+    const searchState = useStoreState((state) => state.search);
     // console.log("loadingState = " , loadingState);
     useEffect(() => {
         status.clear();
         status.setHeaderTitle('Search Results');
         status.setSubTitle('For a Better Tomorrow...');
     });
+
+    const searchy = {
+        page: { current: searchState.page.current },
+        query: { ...searchState.query },
+    };
+
+    console.log('SearchViewer: ', searchy);
+    console.log(JSON.stringify(searchy, undefined, 3));
 
     let output = (
         <FeatureCollection
