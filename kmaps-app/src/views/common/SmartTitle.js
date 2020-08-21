306d6a012c224feebc051d924db30865ee7ab38d
ys2n <ys2n@virginia.edu>
2020-08-21 16:30:48 -0400

    Fixed problem where code was accidentally deleted.

--- kmaps-app/src/views/common/SmartTitle.js
+++ kmaps-app/src/views/common/SmartTitle.js
@@ -52,4 +52,15 @@ export function SmartTitle(props) {
             }
             break;
     }
+
+    // applying LinesEllipsis component...
+    // ys2n: style-kludge as per https://github.com/xiaody/react-lines-ellipsis/issues/59
+    return (
+        <LinesEllipsis
+            style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}
+            maxLine={2}
+            text={smartTitle}
+            basedOn={'words'}
+        />
+    );
 }
