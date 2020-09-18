deb4f4e12365054cd6589540b02f56c1a239ce93
ys2n <ys2n@virginia.edu>
2020-09-18 16:59:20 -0400

    Three-state toggling:  Tree/Advanced/Off Beginnings of FacetChoice popovers Tree navigation:  Subject and Terms not working...
    (cherry picked from commit d5d3aff7b4bd48c48bdb5c6fc3763682594bba6d)

--- kmaps-app/src/main/Main.js
+++ kmaps-app/src/main/Main.js
@@ -17,6 +17,11 @@ import { Error404 } from '../App';
 import SearchContext from '../context/SearchContext';
 import { useStoreRehydrated } from 'easy-peasy';
 import HistoryListener from '../views/History/HistoryListener';
+import FancyTree from '../views/FancyTree';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Container from 'react-bootstrap/Container';
+import { useHistory } from 'react-router';
 
 const stateDefault = {
     kmasset: {
@@ -35,12 +40,28 @@ export function Main(props) {
     const storeReady = useStoreRehydrated();
     const loading = <div className={'loading-msg'}>Loading...</div>;
 
+    let stateList = [];
+    if (state.advanced) {
+        stateList.push('u-ToggleState--advanced');
+    }
+    if (state.tree) {
+        stateList.push('u-ToggleState--tree');
+    }
+    if (!state.tree && !state.advanced) {
+        stateList.push('u-ToggleState--off');
+    }
+
+    const searchClasses = stateList.join(' ');
     const main = (
         <Router basename={'/mandala-om'}>
-            <div id={'l-site__wrap'} className={'l-site__wrap'}>
+            <div
+                id={'l-site__wrap'}
+                className={`l-site__wrap  ${searchClasses}`}
+            >
                 <HistoryListener />
                 <SiteHeader
                     advanced={state.advanced}
+                    tree={state.tree}
                     onStateChange={handleStateChange}
                 />
                 <Hamburger hamburgerOpen={state.hamburgerOpen} />
@@ -75,10 +96,84 @@ export function Main(props) {
                         onStateChange={handleStateChange}
                     />
                 </SearchContext>
+
+                <TreeNav tree={state.tree} />
+
+                <Hamburger hamburgerOpen={state.hamburgerOpen} />
             </div>
         </Router>
     );
 
     const ret = storeReady ? main : loading;
     return ret;
+}
+
+function TreeNav(props) {
+    const openclass = props.tree ? 'open' : 'closed';
+
+    const tabs = (
+        <Container>
+            <aside
+                id="c-columnSearch"
+                className={`c-columnSearch ${openclass} overflow-auto`}
+            >
+                <Tabs defaultActiveKey="places" id="uncontrolled-tab-example">
+                    <Tab eventKey="places" title="Places">
+                        <PlacesTree />
+                    </Tab>
+                    <Tab eventKey="subjects" title="Subjects">
+                        <SubjectsTree />
+                    </Tab>
+                    <Tab eventKey="terms" title="Terms">
+                        <TermsTree />
+                    </Tab>
+                </Tabs>
+            </aside>
+        </Container>
+    );
+    return tabs;
+}
+
+function PlacesTree(props) {
+    return (
+        <FancyTree
+            domain="places"
+            tree="places"
+            descendants={true}
+            directAncestors={false}
+            displayPopup={false}
+            perspective="pol.admin.hier"
+            view="roman.scholar"
+            sortBy="position_i+ASC"
+        />
+    );
+}
+
+function TermsTree(props) {
+    return (
+        <FancyTree
+            domain="terms"
+            tree="terms"
+            descendants={true}
+            directAncestors={false}
+            displayPopup={false}
+            perspective="tib.alpha"
+            view="roman.scholar"
+            sortBy="position_i+ASC"
+        />
+    );
+}
+
+function SubjectsTree(props) {
+    return (
+        <FancyTree
+            domain="subjects"
+            tree="subjects"
+            descendants={true}
+            directAncestors={false}
+            displayPopup={false}
+            view="roman.scholar"
+            sortBy="position_i+ASC"
+        />
+    );
 }
