c94996667292f412774986aeed704900d248f4ab
dukarla <mf8yk@virginia.edu>
2020-09-21 09:50:37 -0600

    css minor updates & removed floating site banner

--- kmaps-app/src/main/SiteHeader/SiteHeader.js
+++ kmaps-app/src/main/SiteHeader/SiteHeader.js
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
 import React, { useLayoutEffect, useState } from 'react';
 import { SearchBar } from '../../search/SearchBar';
-import Headroom from '../../../node_modules/headroom.js'; // see https://wicky.nillia.ms/headroom.js/
+// import Headroom from '../../../node_modules/headroom.js'; // see https://wicky.nillia.ms/headroom.js/
 import './SiteHeader.scss';
 
 export function SiteHeader(props) {
-    useLayoutEffect(() => {
-        var myElement = document.querySelector('.c-site__header');
-        var headroom = new Headroom(myElement, {
-            tolerance: {
-                down: 0,
-                up: 20,
-            },
-            offset: 0,
-        });
-        headroom.init();
-    });
+    //   useLayoutEffect(() => {
+    //       var myElement = document.querySelector('.c-site__header');
+    //      var headroom = new Headroom(myElement, {
+    //          tolerance: {
+    //              down: 0,
+    //              up: 20,
+    //          },
+    //         offset: 20,
+    //    });
+    //    headroom.init();
+    //  });
     const topBar = (
         <section className={'c-site__header'}>
             <Link to={'/home'} className={'u-link-logo'}>
