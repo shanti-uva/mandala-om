7869b73a4dd190c502e1ec49e2f5c3b7e395a42c
ThanGrove <than@virginia.edu>
2020-11-13 16:57:41 -0500

    update to related places for subjects and adding pagers

--- kmaps-app/src/views/Kmaps/KmapsRelPlacesViewer.js
+++ kmaps-app/src/views/Kmaps/KmapsRelPlacesViewer.js
@@ -4,15 +4,25 @@ import { MandalaPopover } from '../common/MandalaPopover';
 import { Col, Container, Row, Tabs, Tab } from 'react-bootstrap';
 import $ from 'jquery';
 import './subjectsinfo.scss';
+import { FeaturePager } from '../common/FeaturePager/FeaturePager';
 
 export function KmapsRelPlacesViewer(props) {
     const { kmap, kmasset } = props;
+    const [startRow, setStartRow] = useState(0);
+    const [pageNum, setPageNum] = useState(0);
+    const [pageSize, setPageSize] = useState(100);
+    const [colSize, setColSize] = useState(20);
+
+    useEffect(() => {
+        $('main.l-column__main').addClass('subjects');
+    }, [kmap]);
 
     const domain = kmap.tree;
     const uid = kmap?.uid;
     const kid = uid?.split('-')[1] * 1;
 
     // Construct Solr query and useSolr call
+    // TODO: generalize to do both places and subjects. This is just for related places of subjects now.
     const q = {
         index: 'terms',
         params: {
@@ -24,11 +34,57 @@ export function KmapsRelPlacesViewer(props) {
                 ') AND tree:places',
             fl: 'tree,uid,uid_i,header',
             sort: 'header ASC',
-            rows: 100,
-            start: 0,
+            rows: pageSize,
+            start: startRow,
         },
     };
-    const placedata = useSolr('subject-8260d-related-places', q);
+    const placedata = useSolr(uid + '-related-places', q);
+
+    const numFound = placedata?.numFound ? placedata?.numFound : 0;
+
+    const pager = {
+        numFound: numFound,
+        getMaxPage: () => {
+            return Math.floor(pager.numFound / pager.getPageSize());
+        },
+        getPage: () => {
+            return pageNum;
+        },
+        setPage: (pg) => {
+            pg = parseInt(pg);
+            if (!isNaN(pg) && pg > -1 && pg <= pager.getMaxPage()) {
+                setPageNum(pg);
+                pager.pgnum = pg;
+            }
+        },
+        setPageSize: (size) => {
+            size = parseInt(size);
+            if (!isNaN(size) && size > 0 && size < 101) {
+                setPageSize(size);
+                pager.pgsize = size;
+            }
+        },
+        getPageSize: () => {
+            return pageSize;
+        },
+        nextPage: () => {
+            pager.setPage(pager.getPage() + 1);
+        },
+        prevPage: () => {
+            pager.setPage(pager.getPage() - 1);
+        },
+        lastPage: () => {
+            pager.setPage(pager.getMaxPage());
+        },
+        firstPage: () => {
+            pager.setPage(0);
+        },
+    };
+
+    useEffect(() => {
+        setStartRow(pageNum * pageSize);
+    }, [pageNum, pageSize]);
+
     // Process into list items
     const placeitems = $.map(placedata.docs, function (item, n) {
         const kid = Math.floor(item.uid_i / 100); // Remove 01 places suffix
@@ -38,23 +94,35 @@ export function KmapsRelPlacesViewer(props) {
             </li>
         );
     });
-    const chunks = chunkIt(placeitems, 25);
+    const chunks = chunkIt(placeitems, colSize);
 
     return (
-        <div className={'related-places'}>
-            <h1>Related Places Tree</h1>
-            <Container>
-                <Row>
-                    {$.map(chunks, function (chk, n) {
-                        return (
-                            <Col md={3}>
-                                <ul>{chk}</ul>
-                            </Col>
-                        );
-                    })}
-                </Row>
-            </Container>
-        </div>
+        <Container fluid className={'c-relplaces-list'}>
+            <h2 className={'row'}>Related Places </h2>
+            {numFound && numFound > pageSize && (
+                <FeaturePager
+                    pager={pager}
+                    position={'top'}
+                    className={'row'}
+                />
+            )}
+            <Row>
+                {$.map(chunks, function (chk, n) {
+                    return (
+                        <Col md={2}>
+                            <ul>{chk}</ul>
+                        </Col>
+                    );
+                })}
+            </Row>
+            {numFound && numFound > pageSize && (
+                <FeaturePager
+                    pager={pager}
+                    position={'bottom'}
+                    className={'row'}
+                />
+            )}
+        </Container>
     );
 }
 
