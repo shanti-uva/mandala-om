5b444a5db511617fce188b6b61408ee5c2316b7d
ThanGrove <than@virginia.edu>
2020-11-18 12:27:08 -0500

    Updates

---
+++ precache-manifest.7a51e962daa8a3488c13ae5d63bc1bb2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11430ddc761d0a7831a27ed9176c6a52",
+    "url": "/mandala-om/index.html"
+  },
+  {
+    "revision": "57d071a39ab7b5410c5e",
+    "url": "/mandala-om/static/css/2.c61feb1d.chunk.css"
+  },
+  {
+    "revision": "f92113502bfca253f621",
+    "url": "/mandala-om/static/css/main.9f30b003.chunk.css"
+  },
+  {
+    "revision": "57d071a39ab7b5410c5e",
+    "url": "/mandala-om/static/js/2.42132226.chunk.js"
+  },
+  {
+    "revision": "35021a14f32b607aef43c0bd02d88731",
+    "url": "/mandala-om/static/js/2.42132226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92113502bfca253f621",
+    "url": "/mandala-om/static/js/main.9181725c.chunk.js"
+  },
+  {
+    "revision": "766b2e71f7b6a3e46927",
+    "url": "/mandala-om/static/js/runtime-main.6b930fe2.js"
+  },
+  {
+    "revision": "be531d813232a92e7619a8e929feae94",
+    "url": "/mandala-om/static/media/shanticon.be531d81.ttf"
+  },
+  {
+    "revision": "bcb9b6f157a6bdcbcefc6888ab285d6d",
+    "url": "/mandala-om/static/media/treebuts.bcb9b6f1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mandala-om/precache-manifest.6035892c56f6606cd3eaddfe776080a2.js"
+  "/mandala-om/precache-manifest.7a51e962daa8a3488c13ae5d63bc1bb2.js"
 );
 
 self.addEventListener('message', (event) => {
