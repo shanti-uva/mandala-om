78fb2f4fcde9cae84598e7df12047ad26aa53772
ThanGrove <than@virginia.edu>
2020-12-02 13:02:55 -0500

    removing added code to term definition

--- kmaps-app/src/views/Terms/TermDefinitions/TermDefinitions.js
+++ kmaps-app/src/views/Terms/TermDefinitions/TermDefinitions.js
@@ -35,125 +35,121 @@ const TermDefinitions = (props) => {
             <div className="sui-termDefinitions__content">
                 {_.orderBy(props.mainDefs, (val) => val.order, 'asc').map(
                     (def, order) => (
-                        <div id={def.id}>
-                            <Tabs key={def.id} defaultActiveKey="definition">
-                                <Tab
-                                    eventKey="definition"
-                                    title={
-                                        <>
-                                            <span className="sui-termDefinitions__tabHeading">
-                                                {`${parseInt(order, 10) + 1}. `}
-                                                Definition{' '}
+                        <Tabs key={def.id} defaultActiveKey="definition">
+                            <Tab
+                                eventKey="definition"
+                                title={
+                                    <>
+                                        <span className="sui-termDefinitions__tabHeading">
+                                            {`${parseInt(order, 10) + 1}. `}
+                                            Definition{' '}
+                                        </span>
+                                        <IconContext.Provider
+                                            value={{
+                                                style: {
+                                                    verticalAlign: 'middle',
+                                                },
+                                            }}
+                                        >
+                                            <TiArrowUnsorted />
+                                        </IconContext.Provider>
+                                    </>
+                                }
+                            >
+                                {ReactHtmlParser(
+                                    def.related_definitions_content_s
+                                )}
+                                {def.related_definitions_author_s &&
+                                    def.related_definitions_language_s && (
+                                        <div className="sui-termDefinitions__extra">
+                                            <span className="sui-termDefinitions__extra-author">
+                                                Author:
+                                            </span>{' '}
+                                            <span className="sui-termDefinitions__extra-author-text">
+                                                {
+                                                    def.related_definitions_author_s
+                                                }
+                                            </span>{' '}
+                                            |{' '}
+                                            <span className="sui-termDefinitions__extra-lang">
+                                                Language:
+                                            </span>{' '}
+                                            <span className="sui-termDefinitions__extra-lang-text">
+                                                {
+                                                    def.related_definitions_language_s
+                                                }
                                             </span>
-                                            <IconContext.Provider
-                                                value={{
-                                                    style: {
-                                                        verticalAlign: 'middle',
-                                                    },
-                                                }}
-                                            >
-                                                <TiArrowUnsorted />
-                                            </IconContext.Provider>
-                                        </>
-                                    }
-                                >
-                                    {ReactHtmlParser(
-                                        def.related_definitions_content_s
+                                        </div>
                                     )}
-                                    {def.related_definitions_author_s &&
-                                        def.related_definitions_language_s && (
-                                            <div className="sui-termDefinitions__extra">
-                                                <span className="sui-termDefinitions__extra-author">
-                                                    Author:
-                                                </span>{' '}
-                                                <span className="sui-termDefinitions__extra-author-text">
-                                                    {
-                                                        def.related_definitions_author_s
-                                                    }
-                                                </span>{' '}
-                                                |{' '}
-                                                <span className="sui-termDefinitions__extra-lang">
-                                                    Language:
-                                                </span>{' '}
-                                                <span className="sui-termDefinitions__extra-lang-text">
-                                                    {
-                                                        def.related_definitions_language_s
-                                                    }
-                                                </span>
-                                            </div>
-                                        )}
-                                </Tab>
-                                <Tab
-                                    eventKey="details"
-                                    title={
-                                        <>
-                                            <span className="sui-termDefinitions__tabHeading">
-                                                Details{' '}
-                                            </span>
-                                            <IconContext.Provider
-                                                value={{
-                                                    style: {
-                                                        verticalAlign: 'middle',
-                                                    },
-                                                }}
-                                            >
-                                                <TiArrowUnsorted />
-                                            </IconContext.Provider>
-                                        </>
-                                    }
-                                >
-                                    <TermDefinitionsDetails def={def} />
-                                </Tab>
-                                <Tab
-                                    eventKey="passages"
-                                    title={
-                                        <>
-                                            <span className="sui-termDefinitions__tabHeading">
-                                                Passages (0){' '}
-                                            </span>
-                                            <IconContext.Provider
-                                                value={{
-                                                    style: {
-                                                        verticalAlign: 'middle',
-                                                    },
-                                                }}
-                                            >
-                                                <TiArrowUnsorted />
-                                            </IconContext.Provider>
-                                        </>
-                                    }
-                                >
-                                    <TermDefinitionsPassages />
-                                </Tab>
-                                <Tab
-                                    eventKey="resources"
-                                    title={
-                                        <>
-                                            <span className="sui-termDefinitions__tabHeading">
-                                                Resources (
-                                                {resourceCounts[def.id]?.all ||
-                                                    0}
-                                                ){' '}
-                                            </span>
-                                            <IconContext.Provider
-                                                value={{
-                                                    style: {
-                                                        verticalAlign: 'middle',
-                                                    },
-                                                }}
-                                            >
-                                                <TiArrowUnsorted />
-                                            </IconContext.Provider>
-                                        </>
-                                    }
-                                >
-                                    <TermDefinitionsResources
-                                        defID={def.id}
-                                        resCounts={resourceCounts}
-                                    />
-                                </Tab>
-                            </Tabs>
-                        </div>
+                            </Tab>
+                            <Tab
+                                eventKey="details"
+                                title={
+                                    <>
+                                        <span className="sui-termDefinitions__tabHeading">
+                                            Details{' '}
+                                        </span>
+                                        <IconContext.Provider
+                                            value={{
+                                                style: {
+                                                    verticalAlign: 'middle',
+                                                },
+                                            }}
+                                        >
+                                            <TiArrowUnsorted />
+                                        </IconContext.Provider>
+                                    </>
+                                }
+                            >
+                                <TermDefinitionsDetails def={def} />
+                            </Tab>
+                            <Tab
+                                eventKey="passages"
+                                title={
+                                    <>
+                                        <span className="sui-termDefinitions__tabHeading">
+                                            Passages (0){' '}
+                                        </span>
+                                        <IconContext.Provider
+                                            value={{
+                                                style: {
+                                                    verticalAlign: 'middle',
+                                                },
+                                            }}
+                                        >
+                                            <TiArrowUnsorted />
+                                        </IconContext.Provider>
+                                    </>
+                                }
+                            >
+                                <TermDefinitionsPassages />
+                            </Tab>
+                            <Tab
+                                eventKey="resources"
+                                title={
+                                    <>
+                                        <span className="sui-termDefinitions__tabHeading">
+                                            Resources (
+                                            {resourceCounts[def.id]?.all || 0}){' '}
+                                        </span>
+                                        <IconContext.Provider
+                                            value={{
+                                                style: {
+                                                    verticalAlign: 'middle',
+                                                },
+                                            }}
+                                        >
+                                            <TiArrowUnsorted />
+                                        </IconContext.Provider>
+                                    </>
+                                }
+                            >
+                                <TermDefinitionsResources
+                                    defID={def.id}
+                                    resCounts={resourceCounts}
+                                />
+                            </Tab>
+                        </Tabs>
                     )
                 )}
             </div>
