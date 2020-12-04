ac2f9e9773fdb58a13c3dd2dc7582b6b9de4d4c2
Derik Ramirez <rderik@gmail.com>
2020-12-04 10:01:56 -0600

    Refactors code for KmapsMap
    Parametrised the GeoserverURL and also the Google API Key both are now
    located in the .evn file.

--- kmaps-app/src/views/KmapsMap/KmapsMap.js
+++ kmaps-app/src/views/KmapsMap/KmapsMap.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
 // Start Openlayers imports
 import {
     Map,
@@ -60,7 +59,6 @@ class KmapsMap extends React.Component {
   constructor (props) {
     super(props);
     this.updateDimensions = this.updateDimensions.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.state = {
       height: props.height || 400,
       width: props.width || 400,
@@ -83,18 +81,23 @@ class KmapsMap extends React.Component {
 
   componentDidMount() {
     this.setState({element: this.refs.inset_map});
-    var dragAndDropInteraction = new DragAndDrop({
-      formatConstructors: [GeoJSON, KML],
+    const geoserverUrl = process.env.REACT_APP_GOSERVER_URL;
+    var map = this.createMap();
+    map.once('postrender',(event) => {
+      this.zoomToFeature(map);
     });
+  }
+
+  createMap() {
     const googleLayer = new GoogleLayer({
       mapTypeId: 'satellite'
     });
     const layer_name = this.state.language_layer;
     const fid = this.state.fid;
-    const geoserverUrl = 'http://dev-places.kmaps.virginia.edu:8080/geoserver/';
+    const geoserverUrl = process.env.REACT_APP_GOSERVER_URL;
     const featureLayer = new TileLayer({
           source: new TileWMSSource({
-            url: 'http://dev-places.kmaps.virginia.edu:8080/geoserver/wms',
+            url: `${geoserverUrl}/wms`,
             params: {
               'LAYERS': 'thl:'+layer_name+'_poly,thl:'+layer_name+'_pt,thl:'+layer_name+'_line' ,
               'STYLES': 'thl_noscale,thl_noscale,thl_noscale',
@@ -115,8 +118,7 @@ class KmapsMap extends React.Component {
       ],
       controls: DefaultControls().extend([
         new ZoomSlider(),
-        new ScaleLine(),
-        new MousePosition()
+        new ScaleLine()
       ]),
       view: new View({
         projection: 'EPSG:900913',
@@ -125,23 +127,21 @@ class KmapsMap extends React.Component {
     });
     var olGM = new OLGoogleMaps({map: map}); // map is the ol.Map instance
     olGM.activate();
-    var that = this;
-    map.once('postrender',(event) => {
-      const cql_filter = `fid=${this.state.fid}`;
-      		const serverUrl = geoserverUrl+
-			'/wfs?service=wfs&version=1.1.0&request=GetFeature&typename=thl:bbox&cql_filter='+cql_filter+
-			'&projection=EPSG:4326&SRS=EPSG:4326&outputFormat=json';
-      fetch(serverUrl).then(res => res.json()).then((result) => {
-        //because we are using WFS V1.1 we need to flip the coordinates
-        const bbox = [result.bbox[1],result.bbox[0],result.bbox[3],result.bbox[2]];
-        var ext = transformExtent(bbox, getProjection('EPSG:4326'), getProjection('EPSG:900913'));
-        map.getView().fit(ext, {size: [this.state.height,this.state.width], padding: [1,1,1,1], constraintResolution: false});
-      });
-    });
+    return map;
   }
 
-  handleClick() {
-    this.updateDimensions();
+  zoomToFeature(map){
+    const cql_filter = `fid=${this.state.fid}`;
+    const geoserverUrl = process.env.REACT_APP_GOSERVER_URL;
+    const serverUrl = geoserverUrl+
+      '/wfs?service=wfs&version=1.1.0&request=GetFeature&typename=thl:bbox&cql_filter='+cql_filter+
+      '&projection=EPSG:4326&SRS=EPSG:4326&outputFormat=json';
+    fetch(serverUrl).then(res => res.json()).then((result) => {
+      //because we are using WFS V1.1 we need to flip the coordinates
+      const bbox = [result.bbox[1],result.bbox[0],result.bbox[3],result.bbox[2]];
+      var ext = transformExtent(bbox, getProjection('EPSG:4326'), getProjection('EPSG:900913'));
+      map.getView().fit(ext, {size: [this.state.height,this.state.width], padding: [1,1,1,1], constraintResolution: false});
+    });
   }
 
   componentWillUnmount() {
@@ -156,13 +156,9 @@ class KmapsMap extends React.Component {
       backgroundColor: '#cccccc',
     };
     return (
-      //<LoadScript
-      //googleMapsApiKey="AIzaSyAXpnXkPS39-Bo5ovHQWvyIk6eMgcvc1q4"
-    //>
       <div>
-        <div tabindex="1" onClick={this.handleClick} style={style} ref="inset_map"></div>
+        <div tabIndex="1" style={style} ref="inset_map"></div>
       </div>
-      //</LoadScript>
     );
   }
 }
