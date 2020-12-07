31f62a3055ab38221e90acf0500d7d7cd74c1bd3
Gerard Ketuma <gketuma@gmail.com>
2020-12-07 00:40:56 -0600

    Remove unused variable.

--- kmaps-app/src/main/ContentMain.js
+++ kmaps-app/src/main/ContentMain.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ContentHeader } from './ContentHeader/ContentHeader';
 import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 import { AudioVideoViewer } from '../views/AudioVideo/AudioVideoViewer';
